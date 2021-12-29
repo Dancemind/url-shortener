491a74010cd6aa161ab38fc88f2bc01605f7766a
Dancemind <accmqq@gmail.com>
2021-12-29 17:52:57 +0300

    enable swagger by springfox v3

--- src/main/java/com/dancemind/urlshortener/controller/UrlDataController.java
+++ src/main/java/com/dancemind/urlshortener/controller/UrlDataController.java
@@ -2,6 +2,7 @@ package com.dancemind.urlshortener.controller;
 
 import com.dancemind.urlshortener.entity.UrlData;
 import com.dancemind.urlshortener.service.UrlDataService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class UrlDataController {
         this.urlDataService = urlDataService;
     }
 
+    @ApiOperation(value = "Returns all urls data")
     @GetMapping
     public List<UrlData> getAllExpenseCategories() {
         return urlDataService.findAllUrlsData();
